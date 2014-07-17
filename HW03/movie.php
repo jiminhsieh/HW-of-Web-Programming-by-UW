db69bd11e61b9fa9046365d0c54b80f75dce141b
JiminHsieh <jiminbass@gmail.com>
2014-07-18 00:35:53 +0800

    Prototype of HW03

--- HW03/movie.php
+++ HW03/movie.php
@@ -1,188 +1,128 @@
 <!DOCTYPE html>
 <html>
    <head>
-      <title>TMNT - Rancid Tomatoes</title>
+      <title>Rancid Tomatoes</title>
       <meta charset="utf-8" />
       <link href="movie.css" type="text/css" rel="stylesheet"/>
    </head>
 
    <body>
       <div id="banner">
-         <center><img src="https://webster.cs.washington.edu/images/rancidbanner.png" alt="Rancid Tomatoes" /></center>
+         <center><img src="rancidbanner.png" alt="Rancid Tomatoes" /></center>
       </div>
-      <h1 id="heading"><center>TMNT (2007)</center></h1>
+<?php
+
+GLOBAL $movie;
+$movie = $_GET["film"];
+list($name, $time, $score) = file("$movie/info.txt");
+
+?>
+
+      <h1 id="heading"><center><? echo $name ?> (<? echo $time?>)</center></h1>
 
       <div id="container"> <!-- for container -->
 
          <div id="overview"> <!-- for overview -->
             <div>
-               <img src="https://webster.cs.washington.edu/images/overview.png" alt="general overview" />
+                  <img src="<? echo $movie ?>/overview.png" alt="general overview" />
             </div>
+
+
             <dl id="overview-font">
-               <dt>STARRING</dt>
-               <dd>Patrick Stewart <br /> Mako <br /> Sarah Michelle Gellar <br /> Kevin Smith</dd>
-               <dt>DIRECTOR</dt>
-               <dd>Kevin Munroe</dd>
-               <dt>RATING</dt>
-               <dd>PG</dd>
-               <dt>THEATRICAL RELEASE</dt>
-               <dd>Mar 23, 2007</dd>
-               <dt>MOVIE SYNOPSIS</dt>
-               <dd>After the defeat of their old arch nemesis, The Shredder, the Turtles have grown apart as a family.</dd>
-               <dt>MPAA RATING</dt>
-               <dd>PG, for animated action violence, some scary cartoon images and mild language</dd>
-               <dt>RELEASE COMPANY</dt>
-               <dd>Warner Bros.</dd>
-               <dt>RUNTIME</dt>
-               <dd>90 mins</dd>
-               <dt>GENRE</dt>
-               <dd>Action/Adventure, Comedies, Childrens, Martial Arts, Superheroes, Ninjas, Animated Characters</dd>
-               <dt>BOX OFFICE</dt>
-               <dd>$54,132,596</dd>
-               <dt>LINKS</dt>
-               <dd>
-               <ul>
-                  <li><a href="http://www.ninjaturtles.com/">The Official TMNT Site</a></li>
-                  <li><a href="http://www.rottentomatoes.com/m/teenage_mutant_ninja_turtles/">RT Review</a></li>
-                  <li><a href="http://www.rottentomatoes.com/">RT Home</a></li>
-               </ul>
-               </dd>
+           
+<?php
+// print Overview Block
+$overview = file("$movie/overview.txt");
+
+$i = 0;
+foreach ($overview as $line) {
+
+   $pattern = array('/:/');
+   $replacement = array(' ');
+   $tmpArray = explode(" ", preg_replace($pattern, $replacement, $line));
+   echo "<dt>$tmpArray[$i]</dt>";
+   $description = array_shift($tmpArray);
+   echo "<dd>";
+   foreach ($tmpArray as $ddLine)
+      echo "$ddLine ";
+   echo "</dd>";
+
+}
+?>
+
             </dl>
+
          </div> <!-- for overview -->
 
          <div id="banner-review">
-            <img src="https://webster.cs.washington.edu/images/rottenlarge.png" alt="Rotten" />
-            <span id="number">33%</span>
+            <img src="<? if ($score >= 60) echo 'freshlarge.png'; else echo 'rottenlarge.png'?>" alt="Rotten" />
+            <span id="number"><?echo $score ?></span>
          </div>
 
-         <div id="review-block"><!-- user review -->
-            <span class="review"> <!-- left column -->
-            <div class="user-block">
-               <p class="quote">
-               <img src="https://webster.cs.washington.edu/images/rotten.gif" alt="Rotten" />
-               <q>Ditching the cheeky, self-aware wink that helped to excuse the concept's inherent corniness, the movie attempts to look polished and 'cool,' but the been-there animation can't compete with the then-cutting-edge puppetry of the 1990 live-action movie.</q>
-               </p>
+         <div id="review-block"> <!-- user review -->
 
-               <p >
-               <img src="https://webster.cs.washington.edu/images/critic.gif" alt="Critic" align="left" />
-               Peter Debruge <br />
-               Variety
-               </p>
-            </div>
+<?php
+// print Review Block
+$allReviews = glob("$movie/review*.txt");
+$numReview = count($allReviews);
 
-            <div class="user-block">
-               <p class="quote">
-               <img src="https://webster.cs.washington.edu/images/fresh.gif" alt="Fresh" />
-               <q>TMNT is a fun, action-filled adventure that will satisfy longtime fans and generate a legion of new ones.</q>
-               </p>
-               <p >
-               <img src="https://webster.cs.washington.edu/images/critic.gif" alt="Critic" align="left"/>
-               Todd Gilchrist <br />
-               IGN Movies
-               </p>
-            </div>
+reviewParser($numReview, $movie);
 
-            <div class="user-block">
-            <p class="quote">
-            <img src="https://webster.cs.washington.edu/images/rotten.gif" alt="Rotten" />
-            <q>It stinks!</q>
-            </p>
-            <p >
-            <img src="https://webster.cs.washington.edu/images/critic.gif" alt="Critic" align="left"/>
-            Jay Sherman (unemployed)
-            </p>
-            </div>
+function reviewParser($numReview, $movie) {
+   $bound = ceil($numReview / 2);  
 
-            <div class="user-block">
-            <p class="quote">
-            <img src="https://webster.cs.washington.edu/images/rotten.gif" alt="Rotten" />
-            <q>The rubber suits are gone and they've been redone with fancy computer technology, but that hasn't stopped them from becoming dull.</q>
-            </p>
-            <p >
-            <img src="https://webster.cs.washington.edu/images/critic.gif" alt="Critic" align="left"/>
-            Joshua Tyler <br />
-            CinemaBlend.com
-            </p>
-            </div>
+   // print each block
+   for ($i = 1; $i <= $numReview; $i++) {
 
-            <div class="user-block">
-            <p class="quote">
-            <img src="https://webster.cs.washington.edu/images/rotten.gif" alt="Rotten" />
-            <q>YOUR REVIEW HERE</q>
-            </p>
-            <p>
-            <img src="https://webster.cs.washington.edu/images/critic.gif" alt="Critic" align="left"/>
-            NAME <br />
-            PUBLICATION
-            </p>
-            </div>
-            </span> <!-- left column -->
+      if ($i == 1 || $i == ($bound + 1)) {
+         echo '<span class="review">';
+      }
 
-            <span class="review"> <!-- right column -->
-            <div>
-            <p class="quote">
-            <img src="https://webster.cs.washington.edu/images/rotten.gif" alt="Rotten" />
-            <q>The turtles themselves may look prettier, but are no smarter; torn irreparably from their countercultural roots, our superheroes on the half shell have been firmly co-opted by the industry their creators once sought to spoof.</q>
-            </p>
-            <p >
-            <img src="https://webster.cs.washington.edu/images/critic.gif" alt="Critic" align="left" />
-            Jeannette Catsoulis <br />
-            New York Times
-            </p>
-            </div>
+      // Deal the problem, when reviews more than 10 
+      if ($numReview >= 10) {
+         if ($i < 10)
+            $location = $movie . "/review" . '0'. $i .".txt";
+         else 
+            $location = $movie . "/review" . $i .".txt";
+      } else {
+         $location = $movie . "/review" . $i .".txt";
+      }
+      list($review, $score, $name, $from) = file($location);
 
-            <div class="user-block">
-            <p class="quote">
-            <img src="https://webster.cs.washington.edu/images/rotten.gif" alt="Rotten" />
-            <q>Impersonally animated and arbitrarily plotted, the story appears to have been made up as the filmmakers went along.</q>
-            </p>
-            <p >
-            <img src="https://webster.cs.washington.edu/images/critic.gif" alt="Critic" align="left" />
-            Ed Gonzalez <br />
-            Slant Magazine
-            </p>
-            </div>
-            
-            <div>
-            <p class="quote">
-               <img src="https://webster.cs.washington.edu/images/fresh.gif" alt="Fresh" />
-            <q>The striking use of image and motion allows each sequence to leave an impression. It's an accomplished restart to this franchise.</q>
-            </p>
-            <p >
-               <img src="https://webster.cs.washington.edu/images/critic.gif" alt="Critic" align="left" />
-               Mark Palermo <br />
-               Coast (Halifax, Nova Scotia)
-            </p>
-            </div>
-            
-            <p class="quote">
-            <img src="https://webster.cs.washington.edu/images/rotten.gif" alt="Rotten" />
-            <q>The script feels like it was computer generated. This mechanical presentation lacks the cheesy charm of the three live action films.</q>
-            </p>
-            <p >
-            <img src="https://webster.cs.washington.edu/images/critic.gif" alt="Critic" align="left" />
-            Steve Rhodes <br />
-            Internet Reviews
-            </p>
-            
-            <p class="quote">
-               <img src="https://webster.cs.washington.edu/images/rotten.gif" alt="Rotten" />
-               <q>YOUR REVIEW HERE</q>
-            </p>
-            <p >
-            <img src="https://webster.cs.washington.edu/images/critic.gif" alt="Critic" align="left" />
-            NAME <br />
-            PUBLICATION
-            </p>
-         </span> <!-- right column -->
-         </div><!-- user review --> 
-            <div id="page"><p>(1-10) of 88</p></div>
-            </div> <!-- for container -->
-            
+
+      // print out quote
+      echo '<p class="quote">';
+      if ($score == "ROTTEN\n") {
+         echo '<img src="rotten.gif" alt="Rotten/>"';
+      } else {
+         echo '<img src="fresh.gif" alt="Fresh/>"';
+      }
+      echo '<q>' . $review . '</q>';
+      echo '</p>';
+
+      // print out reviewer
+      echo '<p>';
+      echo '<img src="critic.gif" alt="Critic" align="left" />';
+      echo $name .' <br />';
+      echo $from;
+      echo '</p>';
+
+      if ($i == $bound || $i == $numReview) {
+         echo '</span>';
+      }
+
+   }
+
+}
+?>
+      </div> <!-- user review --> 
+      <div id="page"><p>(1-10) of 88</p></div>
+      </div> <!-- for container -->
+
 
       <div align="right">
-               <a href="https://webster.cs.washington.edu/validate-html.php"><img src="https://webster.cs.washington.edu/images/w3c-html.png" alt="Valid HTML5" /></a><br>
-               <a href="https://webster.cs.washington.edu/validate-css.php"><img src="https://webster.cs.washington.edu/images/w3c-css.png" alt="Valid CSS" /></a>
+               <a href="https://webster.cs.washington.edu/validate-html.php"><img src="w3c-html.png" alt="Valid HTML5" /></a><br>
+               <a href="https://webster.cs.washington.edu/validate-css.php"><img src="w3c-css.png" alt="Valid CSS" /></a>
       </div>
    </body>
 </html>
