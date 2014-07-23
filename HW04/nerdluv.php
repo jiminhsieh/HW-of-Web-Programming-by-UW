69492b7f5a607f9ba3d62cf9a1ecfd19535af498
JiminHsieh <jiminbass@gmail.com>
2014-07-23 20:21:31 +0800

    Part of HW04

---
+++ HW04/nerdluv.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+	<!--
+	CSE 154, Homework 4 (NerdLuv)
+	This provided file is the front page that links to two of the files
+	you are going to write, signup.php and matches.php.
+	You can modify this file as necessary to move redundant code out to common.php.
+	-->
+	
+	<head>
+		<title>NerdLuv</title>
+		
+		<meta charset="utf-8" />
+		
+		<!-- instructor-provided CSS and JavaScript links; do not modify -->
+		<link href="https://webster.cs.washington.edu/images/nerdluv/heart.gif" type="image/gif" rel="shortcut icon" />
+		<link href="https://webster.cs.washington.edu/css/nerdluv.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+		<div id="bannerarea">
+			<img src="https://webster.cs.washington.edu/images/nerdluv/nerdluv.png" alt="banner logo" /> <br />
+			where meek geeks meet
+		</div>
+
+		<div>
+			<h1>Welcome!</h1>
+
+			<ul>
+				<li>
+					<a href="signup.php">
+						<img src="https://webster.cs.washington.edu/images/nerdluv/signup.gif" alt="icon" />
+						Sign up for a new account
+					</a>
+				</li>
+
+				<li>
+					<a href="matches.php">
+						<img src="https://webster.cs.washington.edu/images/nerdluv/heartbig.gif" alt="icon" />
+						Check your matches
+					</a>
+				</li>
+			</ul>
+		</div>
+
+		<div>
+			<p>
+				This page is for single nerds to meet and date each other!  Type in your personal information and wait for the nerdly luv to begin!  Thank you for using our site.
+			</p>
+			
+			<p>
+				Results and page (C) Copyright NerdLuv Inc.
+			</p>
+			
+			<ul>
+				<li>
+					<a href="nerdluv.php">
+						<img src="https://webster.cs.washington.edu/images/nerdluv/back.gif" alt="icon" />
+						Back to front page
+					</a>
+				</li>
+			</ul>
+		</div>
+
+		<div id="w3c">
+			<a href="https://webster.cs.washington.edu/validate-html.php"><img src="https://webster.cs.washington.edu/images/w3c-html.png" alt="Valid HTML" /></a>
+			<a href="https://webster.cs.washington.edu/validate-css.php"><img src="https://webster.cs.washington.edu/images/w3c-css.png" alt="Valid CSS" /></a>
+		</div>
+	</body>
+</html>
---
+++ HW04/signup.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+	<!--
+	CSE 154, Homework 4 (NerdLuv)
+	This provided file is the front page that links to two of the files
+	you are going to write, signup.php and matches.php.
+	You can modify this file as necessary to move redundant code out to common.php.
+	-->
+	
+	<head>
+		<title>NerdLuv</title>
+		
+		<meta charset="utf-8" />
+		
+		<!-- instructor-provided CSS and JavaScript links; do not modify -->
+		<link href="https://webster.cs.washington.edu/images/nerdluv/heart.gif" type="image/gif" rel="shortcut icon" />
+		<link href="https://webster.cs.washington.edu/css/nerdluv.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+		<div id="bannerarea">
+			<img src="https://webster.cs.washington.edu/images/nerdluv/nerdluv.png" alt="banner logo" /> <br />
+			where meek geeks meet
+		</div>
+
+		<div> <!-- Matches Form -->
+			<form action= method=>
+			<fieldset>
+				<ul>
+					<strong>Name:</strong>
+				</ul>
+				<input type="text" size="16"/>
+				<br/>
+
+				<ul>
+					<strong>Gender:</strong>
+				</ul>
+				<input type="radio"/> Male
+				<input type="radio" checked="checked"/> Female
+				<br/>
+
+				<ul>
+					<strong>Age:</strong>
+				</ul>
+				<input type="text" size="6"/>
+				<br/>
+
+				<ul>
+					<strong>Personality type:</strong>
+				</ul>
+				<input type="text" size="6"/>
+				<br/>
+
+				<ul>
+					<strong>Favorite OS:</strong>
+				</ul>
+				<select>
+					<option>Windows</option>
+					<option>Mac OS X</option>
+					<option>Linux</option>
+				</select>
+				<br/>
+
+				<ul>
+					<strong>Seeking age:</strong>
+				</ul>
+				<input type="text" size="6" placeholder="min"/>to
+				<input type="text" size="6" placeholder="max"/> 
+				<br/>
+				
+			</fieldset>
+			</form>
+		</div> <!-- Matches Form -->
+
+		<div>
+			<p>
+				This page is for single nerds to meet and date each other!  Type in your personal information and wait for the nerdly luv to begin!  Thank you for using our site.
+			</p>
+			
+			<p>
+				Results and page (C) Copyright NerdLuv Inc.
+			</p>
+			
+			<ul>
+				<li>
+					<a href="nerdluv.php">
+						<img src="https://webster.cs.washington.edu/images/nerdluv/back.gif" alt="icon" />
+						Back to front page
+					</a>
+				</li>
+			</ul>
+		</div>
+
+		<div id="w3c">
+			<a href="https://webster.cs.washington.edu/validate-html.php"><img src="https://webster.cs.washington.edu/images/w3c-html.png" alt="Valid HTML" /></a>
+			<a href="https://webster.cs.washington.edu/validate-css.php"><img src="https://webster.cs.washington.edu/images/w3c-css.png" alt="Valid CSS" /></a>
+		</div>
+	</body>
+</html>
